fc37e3d9ecace6d961d548217f30ee861e39d1e9
maratustra <maratustraa@gmail.com>
2021-08-03 16:25:31 +0500

    fixed marks for main block and started recommendations block

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
 
   let menuButton = $('.menu-button');
+  let closeButton = $('.navbar-top__close');
 
   menuButton.on('click', function () {
     $('.navbar-bottom').toggleClass('navbar-bottom--visible');
+    $('.navbar-top__close').toggleClass('navbar-bottom--visible--button');
+    $('.menu-button').toggleClass('navbar-top--hidden');
+  });
+
+  closeButton.on('click', function () {
+    $('.navbar-bottom').toggleClass('navbar-bottom--visible');
+    $('.navbar-top__close').toggleClass('navbar-bottom--visible--button');
+    $('.menu-button').toggleClass('navbar-top--hidden');
   });
 
 });
